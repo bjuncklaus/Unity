3a8d8150a9a3a4409b8f8605b8c5691efd3bbbe0
bjuncklaus <bjuncklaus@gmail.com>
2017-09-10 20:43:39 -0300

    Added counter and game over labels.

--- example/Move-A-Ball/Assets/Scripts/PlayerController.cs
+++ example/Move-A-Ball/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
+	
 	public float speed;
+
+	public Text countText;
+	public Text winText;
+
+	private int count;
 
 	private Rigidbody rb;
 
 	void Start() {
 		speed = 15;
+		count = 0;
+		UpdateCountText ();
+		winText.text = "";
 		rb = GetComponent<Rigidbody> ();
 	}
 
@@ -19,5 +29,21 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+//		Destroy (other.gameObject);
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count++;
+			UpdateCountText ();
+		}
+	}
+
+	void UpdateCountText() {
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 14) {
+			winText.text = "You Win";
+		}
 	}
 }
