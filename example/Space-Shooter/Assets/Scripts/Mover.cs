c86032d6caa5bf4d1329fb2c68a2dcb8a3b802ba
bjuncklaus <bjuncklaus@gmail.com>
2017-09-12 20:13:06 -0300

    Added shooting, not functional.

---
+++ example/Space-Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+
+	void Start() {
+		speed = 15;
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = GetComponent<Transform> ().forward * speed ;
+	}
+
+	void FixedUpdate () {
+		if (rb.position.z >= 45) {
+			// Destroy the bolt
+		}
+	}
+}
