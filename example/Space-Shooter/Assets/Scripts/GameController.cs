c33945ac1964bcbfc4d7655c27cd880632c47b74
bjuncklaus <bjuncklaus@gmail.com>
2017-09-14 20:38:15 -0300

    Added GameController and SpawnWaves function.

---
+++ example/Space-Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	void SpawnWaves() {
+		Vector3 spawnPosition = new Vector3 (Random.Range (-6, 6), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpawnWaves ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
