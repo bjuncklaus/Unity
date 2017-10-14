41c10f23687ec930d31ecb29141f897c0b2b9ae6
bjuncklaus <bjuncklaus@gmail.com>
2017-10-13 21:35:55 -0300

    Refactoring the polymon controller algorithm.

---
+++ example/RPG_Tutorial/Assets/Scripts/Controller/PolymonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PolymonController : MonoBehaviour {
+
+    Transform target;
+    public float speed;
+    public float distanceFromPlayer;
+
+    void Start() {
+        target = PlayerManager.instance.player.transform;
+    }
+
+    void FixedUpdate() {
+        //float distance = Vector3.Distance(target.position, transform.position);
+        FaceTarget();
+		float step = speed * Time.deltaTime;
+        //Vector3 destination = new Vector3(target.position.x + distanceFromPlayer, transform.position.y, target.position.z + distanceFromPlayer);
+        Vector3 destination = target.position - target.forward;
+        destination.y = target.position.y + target.localScale.y + distanceFromPlayer;
+        //transform.position = Vector3.MoveTowards(transform.position, destination, step);
+        transform.position = Vector3.Lerp(transform.position, destination, step);
+    }
+
+    void FaceTarget() {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+}
